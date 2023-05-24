8d05c91232f87d0041884df4f6f7c946e27a687f
unknown <aleksejignatov088@gmail.com>
2023-05-24 19:11:17 +0300

    Add 404.js 	new file:   pages/404.js

---
+++ pages/404.js
@@ -0,0 +1,13 @@
+import MainContainer from '../components/MainContainer.js';
+
+export default function Error() {
+    return (
+        <MainContainer
+        title={'Страница не найдена'}
+        >
+            <div>
+                <h1>Страница не найдена</h1>
+            </div>
+        </MainContainer>
+    );
+}
