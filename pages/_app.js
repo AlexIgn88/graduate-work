9c2e939aa14afdd9358c45dbf1f2a7ca0b81db01
unknown <aleksejignatov088@gmail.com>
2023-05-24 13:35:28 +0300

    The first commit. Pages created 	new file:   package.json 	new file:   pages/_app.js 	new file:   pages/about.js 	new file:   pages/blog.js 	new file:   pages/contact.js 	new file:   pages/forum.js 	new file:   pages/index.js 	new file:   pages/places.js 	new file:   pages/store.js 	new file:   pnpm-lock.yaml

---
+++ pages/_app.js
@@ -0,0 +1,11 @@
+// import "../styles/style.css";
+// import Head from 'next/head';
+
+export default function MyApp({ Component, pageProps }) {
+  return <>
+    {/* <Head>
+      <link rel="icon" href="favicon.png" />
+    </Head> */}
+    <Component {...pageProps} />
+  </>
+}
---
+++ pages/index.js
@@ -0,0 +1,14 @@
+// import Head from 'next/head';
+
+export default function App() {
+    return (
+        <>
+            {/* <Head>
+                <title>The table of JSONPlaceholder`s users</title>
+            </Head> */}
+            <div>
+                <h1>Главная</h1>
+            </div>
+        </>
+    );
+}
