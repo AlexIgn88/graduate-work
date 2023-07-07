014a2020c9d750aa7c3a1767b2898e2ffed06546
Alex <user@user.com>
2023-07-07 22:16:22 +0300

    Правки в код 	modified:   components/Navbar.js 	modified:   package.json

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,5 +1,5 @@
 import pages from '../components/pages';
-import Login from '../components/login';
+import Login from '../components/Login';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 import { useSession } from 'next-auth/react';
