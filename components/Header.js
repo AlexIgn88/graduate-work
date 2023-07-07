e6cb45bf400941a51efdeced1cb6561afa07d6ee
Alex <user@user.com>
2023-07-07 21:54:59 +0300

    Правки в Header.js 	modified:   components/Header.js

--- components/Header.js
+++ components/Header.js
@@ -1,9 +1,7 @@
 import Navbar from '../components/Navbar.js';
-// import Login from '../components/login.js';
 
 export default function Header() {
     return <header>
         <Navbar />
-        {/* <Login /> */}
     </header>
 }
