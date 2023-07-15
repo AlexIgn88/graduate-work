c0ea90a3507df3ad12517024683b51e6f0d7093a
Alex <user@user.com>
2023-07-15 12:23:22 +0300

    Id and emailVerified disabled in usersColumns.js

--- includes/usersColumns.js
+++ includes/usersColumns.js
@@ -1,12 +1,12 @@
 const userColumns = [
-    { name: 'Id', getVal: ({ id }) => id },
+    // { name: 'Id', getVal: ({ id }) => id },
     // { name: 'Id', getVal: ({ id }) => id, setVal: val => ({ id: val }) },
 
     { name: 'Name', getVal: ({ name }) => name, setVal: val => ({ name: val }) },
     { name: 'Email', getVal: ({ email }) => email, setVal: val => ({ email: val }) },
 
 
-    { name: 'emailVerified', getVal: ({ emailVerified }) => emailVerified },
+    // { name: 'emailVerified', getVal: ({ emailVerified }) => emailVerified },
     // { name: 'emailVerified', getVal: ({ emailVerified }) => emailVerified, setVal: val => ({ emailVerified: val }) },
 
     { name: 'image', getVal: ({ image }) => <img src={image} alt="avatar"></img> },
