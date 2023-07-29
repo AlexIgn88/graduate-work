5d991b5b04f23c36921745942136a3c892982c8a
unknown <aleksejignatov088@gmail.com>
2023-07-29 06:44:24 +0300

    index.js fixes

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,46 @@
 import Head from "next/head";
+import {  Box, Flex  } from '@chakra-ui/react';
+
+import Image from 'next/image';
+import background from '../public/img/HomePage/background.jpg';
 
 export default function HomePage() {
     return <>
         <Head>
             <title>Туристическое агентство &quot;Исторические маршруты и заповедные места&quot;</title>
         </Head>
-        <div className="home-page">
-            <div>
-                <div className="home-page-title">
-                    <div>ИСТОРИЧЕСКИЕ МАРШРУТЫ</div>
-                    <div>ЗАПОВЕДНЫЕ МЕСТА</div>
-                </div>
-            </div>
-        </div>
+        <Box
+            className='home-page'
+            // display={'flex'}
+            // alignItems={'center'}
+            // justifyContent={'flex-start'}
+            position="relative"
+            height="100vh"
+        >
+
+            <Image
+                src={background}
+                alt='Вид с Демерджи на Чатыр-даг на закате'
+                placeholder="blur"
+                style={{ objectFit: 'cover' }}
+                fill={true}
+            />
+            <Box
+                className='home-page-title'
+                left={{ base: '30px', '2xl': '200px', xl: '150px', lg: '100px', md: '80px', sm: '50px' }}
+                color={'white'}
+                fontSize={'7vmin'}
+                fontWeight={'bold'}
+                fontFamily={'monospace'}
+                position={'absolute'}
+                top={'300px'}
+                bottom={'0'}
+                right={'0'}
+            >
+                <Box>ИСТОРИЧЕСКИЕ МАРШРУТЫ</Box>
+                <Box>ЗАПОВЕДНЫЕ МЕСТА</Box>
+            </Box>
+
+        </Box >
     </>
 }
