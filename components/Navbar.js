af3d9d08ca2939336dc62eb9800ccdcfc677d0d1
unknown <aleksejignatov088@gmail.com>
2023-07-29 03:46:47 +0300

    router.pathname.split('/')[1] === src.split('/')[1] 	modified:   components/Navbar.js

--- components/Navbar.js
+++ components/Navbar.js
@@ -7,19 +7,22 @@ import Login from '../components/Login';
 export default function Navbar() {
     const router = useRouter();
     const { data: session } = useSession();
+
+    // console.log('router.pathname', router.pathname);
+
     return <nav>
         <ul className="navbar">
             {pages.filter(page => page?.restricted
                 ? page.restricted(session)
                 : true)
                 .map(({ name, src }) =>
-                    <li key={name} className={router.pathname === src
+                    <li key={name} className={router.pathname.split('/')[1] === src.split('/')[1]
                         ? 'active'
                         : ''}>
                         <Link href={src} className="link">{name}</Link>
                     </li>
                 )}
-                <li><Login /></li>
+            <li><Login /></li>
         </ul>
     </nav>;
 }
