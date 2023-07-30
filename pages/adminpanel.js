a96a2f7cef00ff3ad38313e33295c7f460f8c310
unknown <aleksejignatov088@gmail.com>
2023-07-30 14:28:12 +0300

    work with adminpanel

--- pages/adminpanel.js
+++ pages/adminpanel.js
@@ -10,6 +10,20 @@ import { Box, Flex, Spacer, Heading, Button, ButtonGroup, Input } from "@chakra-
 import { h1HeadersFontSize, h2HeadersFontSize, h3HeadersFontSize, textFontSize } from '../displayParameters/fontParameters';
 import { HeadingForPage } from '../components/ElemsForPages';
 
+// import { SWRConfig } from 'swr';
+// import GetData from '../components/GetData';
+// import { useSession, signIn } from 'next-auth/react';
+
+// import userColumns from '../data/usersColumnsForAdminPanel';
+// import EditableAdminpanel from '../components/EditableAdminpanel';
+// import Head from "next/head";
+// import { Box, Flex, Spacer, Heading, Button, ButtonGroup, Input } from "@chakra-ui/react";
+// import { h1HeadersFontSize, h2HeadersFontSize, h3HeadersFontSize, textFontSize } from '../displayParameters/fontParameters';
+// import { marginParameters } from '../displayParameters/marginParameters';
+// import { flexDirection } from '../displayParameters/flexParameters';
+// import { HeadingForPage } from '../components/ElemsForPages';
+
+
 // в апи не id: +id 
 
 const
@@ -56,3 +70,34 @@ export default function AdminPanelPage() {
   </>
 
 }
+
+// export default function AdminPanelPage() {
+
+//   const API_URL = '/api/admin/user/',
+//     { data: session } = useSession();
+
+//   return <>
+//     <Head>
+//       <title>Админ-панель</title>
+//     </Head>
+//     <Box m={marginParameters} className="admin-page">
+//       {!session && <HeadingForPage element={'h1'} content={'Для просмотра этой страницы необходимо быть админом'} />}
+
+//       {session && <>
+
+//         <SWRConfig>
+//           {/* <SWRConfig value={{ fallback }}> */}
+//           <GetData url={API_URL}>
+//             <EditableAdminpanel columns={userColumns} />
+//           </GetData>
+//         </SWRConfig>
+
+//       </>
+
+
+
+
+//       }
+//     </Box>
+//   </>
+// }
